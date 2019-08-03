ddd574243a1e111075ef0f0bd40e66191f9988cb
20jin <kekeke33@gmail.com>
2019-08-03 23:32:59 +0900

    오디오 스티림이 2개 이상일때 처리 추가

--- src/Main.py
+++ src/Main.py
@@ -17,8 +17,8 @@ def encoding(cur_dir):
     mkv_files = [file for file in file_list if file.endswith(".mkv")]
     print(f"mkv리스트 : {mkv_files}")
     if len(mkv_files) > 1:
-        # TODO : MKV 파일이 여러개일때 처리못함
         print("mkv file 이 두개 이상존재합니다.")
+        step2(cur_dir)
         return
     mkv_full_path = os.path.join(cur_dir, mkv_files[0])
     print(mkv_full_path)
@@ -26,38 +26,61 @@ def encoding(cur_dir):
     print(audio_stream)
 
     if len(audio_stream) == 1:
-        # Done : Audio 스트림이 DTS하나일때
+        # Done : 1 DTS Audio Stream
         if 'dts' in audio_stream[0]:
-            #처리
-            filename, extension = os.path.splitext(mkv_full_path)
-            origin_name = filename + '_origin' + extension
-            shutil.move(mkv_full_path, origin_name)
-            #ffmpeg -i "xxx.mkv" -map 0:0 -map 0:1 -map 0:1 -c:v copy -c:a:0 ac3 -ac 6 -b:a:0 640k -c:a:1 copy xxx.mkv
-            ffmpeg_proc = subprocess.Popen(args=[f'ffmpeg -i \"{origin_name}\" -map 0:v -map 0:a -map 0:a -c:v copy -c:a:0 ac3 -ac 6 -b:a:0 640k -c:a:1 copy \"{mkv_full_path}\"'], shell=True)
-            ffmpeg_proc.wait(timeout=20*60)
-            try:
-                if os.path.getsize(origin_name) <= os.path.getsize(mkv_full_path):
-                    if os.path.isfile(origin_name):
-                        os.remove(origin_name)
-                        print(f"{origin_name} 삭제됨")
-                    done(cur_dir)
-            except os.error:
-                print("error in getsize")
+            makeAC3fromDTS(cur_dir, mkv_full_path)
         else:
-            # Done: Audio 스트림 하나인데 DTS아닐때
-            print("dts가 없으므로 완료처리")
+            # Done: 1 Audio Stream and Not DTS codec
+            print("just move directory")
             done(cur_dir)
-    # TODO : Audio 스트림이 여러개일 경우
 
+    # TODO : Audio 스트림이 여러개일 경우
+    else:
+        if 'dts' in audio_stream[0] and 'eng' in audio_stream[0]:
+            makeAC3fromDTS(cur_dir, mkv_full_path)
+        else:
+            step2(cur_dir)
+
+def makeAC3fromDTS(cur_dir, mkv_full_path, is_test=False):
+    """
+     1 dts codec --> 1 ac3
+                 \-> 1 dts
+
+    :param cur_dir:
+    :param mkv_full_path:
+    :return:
+    """
+    filename, extension = os.path.splitext(mkv_full_path)
+    origin_name = filename + '_origin' + extension
+    shutil.move(mkv_full_path, origin_name)
+    # ffmpeg -i "xxx.mkv" -map 0:0 -map 0:1 -map 0:1 -c:v copy -c:a:0 ac3 -ac 6 -b:a:0 640k -c:a:1 copy xxx.mkv
+    ffmpeg_proc = subprocess.Popen(args=[
+        f'ffmpeg -i \"{origin_name}\" -map 0:0 -map 0:1 -map 0:1 -c:v copy -c:a:0 ac3 -ac 6 -b:a:0 640k -c:a:1 copy \"{mkv_full_path}\"'],
+                                   shell=True)
+    ffmpeg_proc.wait(timeout=20 * 60)
+
+    if not is_test:
+        try:
+            if os.path.getsize(origin_name) <= os.path.getsize(mkv_full_path):
+                if os.path.isfile(origin_name):
+                    os.remove(origin_name)
+                    print(f"{origin_name} 삭제됨")
+                done(cur_dir)
+        except os.error:
+            print("error in getsize")
 
 def done(arg_dir):
-    #move
-    if os.path.isdir(arg_dir) and os.path.exists(arg_dir):
-        shutil.move(arg_dir, '/mnt/y/영화/')
+    movedir(arg_dir, '/mnt/y/영화/')
+    print("처리 완료")
+
+#처리할 수 없음
+def step2(arg_dir):
+    movedir(arg_dir, '/mnt/y/encoding2/')
+
+def movedir(path, dest):
+    if os.path.isdir(path) and os.path.exists(path):
+        shutil.move(path, dest)
         print("처리 완료")
-
-
-
 
 def getAudioStreamList(mkv_filename):
     p1 = subprocess.Popen(args=['ffmpeg', '-i', mkv_filename], stderr=subprocess.PIPE,
